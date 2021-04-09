35fe0096a73ffbf5b7454dfd3a87f22375866836
LeeSueM <liarchms@outlook.com>
2021-04-09 22:53:40 +0800

    9. Palindrome Number slow but simple

---
+++ 9.Palindrome_Number.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+; https://leetcode.com/problems/palindrome-number/
+(define (is-palindrome x)
+	(-> exact-integer? boolean?)
+	(let
+		([num_str (~v x)])
+		(equal? (list->string (reverse (string->list num_str))) num_str)))
+
+(is-palindrome 121)

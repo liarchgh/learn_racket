9f59c10d9c5abbf8c7839985d6d1eb7ea50daf26
XavierMain <liarchms@outlook.com>
2024-06-16 02:17:18 +0800

    8.4

--- test/test.rkt
+++ test/test.rkt
@@ -234,12 +234,24 @@
 
 ; 8.3
 
+; (define-values (in out) (make-pipe))
+; (define tl '(1 3 "sdf"))
+; (write tl out)
+; (define tl2 (read in))
+; (display tl out)
+; (define tl3 (read in))
+; (println tl)
+; (printf "read from write:~v, equal with origin:~a \n" tl2 (equal? tl tl2))
+; (printf "read from display:~v, equal with origin:~a \n" tl3 (equal? tl tl3))
+
+
+; 8.4
+
+; serializable-struct
+(require racket/serialize)
+(serializable-struct posn (x y) #:transparent)
+(deserialize (serialize (posn 1 2)))
+(write (serialize (posn 1 2)))
 (define-values (in out) (make-pipe))
-(define tl '(1 3 "sdf"))
-(write tl out)
-(define tl2 (read in))
-(display tl out)
-(define tl3 (read in))
-(println tl)
-(printf "read from write:~v, equal with origin:~a \n" tl2 (equal? tl tl2))
-(printf "read from display:~v, equal with origin:~a \n" tl3 (equal? tl tl3))
+(write (serialize (posn 1 2)) out)
+(deserialize (read in))
